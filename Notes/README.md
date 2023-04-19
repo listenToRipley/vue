5aa14d627e2988bb1cfb6a391fb3e8c5c1624faa
Natalie Kendrick <nken@lle-nken-up.lle.rochester.edu>
2023-04-18 20:13:20 -0400

    almost complete notes app

--- Notes/README.md
+++ Notes/README.md
@@ -1,10 +1,38 @@
 # Section 2 - Notes
 
-This template should help get you started developing with Vue 3 in Vite.
+You can use `:` in front an attribute to bind it in place.
+
+## Directives
+
+Instructions for Vue to do specific actions placed on an HTML element.
+
+You provide the v-directiveName after the opening tag element.
+
+Vue [comes with a number of these built in options](https://vuejs.org/api/built-in-directives.html)
+
+### v-if vs. v-show
+
+If will completely remove the element and only display it when the case is supported.
+
+Show will still have the element with in the html, but hidden.
+
+The use case with depend on how accessible the associated content should be.
+
+### v-model
+
+This will provide state for two-way binding. This should be associated with a specific variable so that state will sync.
+
+### v-for
+
+This creates a "forEach" loop, you will provide the array and then shorthand for it.
+
+Just like "forEach", you must include a key. If you are using a key from the associated array, make sure you "bind" the value by placing a `:` in front of the word `key`, eg `:key="array.id"`
 
 ## Starting Notes
 
 The notes this file came with on install
+
+This template should help get you started developing with Vue 3 in Vite.
 
 ### Recommended IDE Setup
 
--- Notes/src/App.vue
+++ Notes/src/App.vue
@@ -1,47 +1,170 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import {ref} from "vue";
+
+  const showModal = ref(false);
+  const newNote = ref("");
+  const notes = ref([]);
+
+  const getRandomColor = () => {
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  }
+
+  const addNote = () => {
+    notes.value.push({
+      id: Math.floor(Math.random() * 100000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor()
+    });
+
+    showModal.value = false;
+    newNote.value ="";
+  };
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <!-- {{ newNote }} -->
+        <textarea v-model="newNote" name="notes" id="notes"></textarea>
+        <button @click="addNote">Add Note</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <header>
+
+        <h1>
+          Notes
+        </h1>
+        <button id="createNoteBtn" @click="showModal = true">
+          +
+        </button>
+      </header>
+    </div>
+
+    <div class="cards-container">
+      <div 
+        v-for="note in notes" 
+        :key="note.id"
+        class="card" 
+        :style="{ backgroundColor: note.backgroundColor }"
+        >
+        <p class="main-text">{{ note.text }}</p>
+        <p class="date">{{ note.date }} </p>
+      </div>
+
+    </div>
   </main>
 </template>
 
-<style scoped>
+<style scope>
+
+main {
+  height: 100vh;
+  width: 100vw;
+  padding: 20px;
+}
+
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
 header {
-  line-height: 1.5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  font-weight: bold;
+  margin-top: 25px;
+  font-size: 75px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#createNoteBtn {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
 }
+
+.card {
+  width: 225px;
+  height: 225px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.main-text {
+  margin-top: 5px;
+  margin-left: 10px;
+}
+
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
+  margin: 10px;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1%;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+  border-radius: 5px;
+}
+
+textarea {
+  width: 100%;
+  height: 200px;
+  padding: 5px;
+  font-size: 20px;
+}
+
 </style>
