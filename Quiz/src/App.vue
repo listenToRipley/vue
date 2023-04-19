2249cd1f75d6de364cdeff4420677ce8071a957f
listenToRipley <natalie.m.kendrick@gmail.com>
2023-04-19 16:36:14 -0400

    make singular component

--- Quiz/src/App.vue
+++ Quiz/src/App.vue
@@ -2,6 +2,8 @@
 import q from "../data/quiz.json";
 import {ref, watch } from "vue";
 
+import Card from "./compontents/Card.vue"
+
 const quizzes = ref(q)
 
 const search = ref("")
@@ -23,19 +25,8 @@ watch(search, () => {
     </header>
 
     <div class="options-cards">
-      <div 
-        v-for="quiz in quizzes" 
-        :key="quiz.id" 
-        class="card"
-        >
-        <img :src="quiz.img" :alt="quiz.name">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }}  Questions </p>
-        </div>
-      </div>
+      <Card/>
     </div>
-
   </div>
 
 </template>
@@ -67,37 +58,12 @@ header input {
   border-radius: 5px;
 }
 
-/* CARD SECTION */
 
 .options-cards {
   display: flex;
   flex-wrap: wrap;
   margin-left: 25px;
   margin-top: 40px;
-}
-
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 
 </style>
