9fb52cf92ae1a3397790a43d47b8b1b1cbfc315f
listenToRipley <natalie.m.kendrick@gmail.com>
2023-05-17 14:56:06 -0400

    add methods

--- Oreilly/Project1/src/index.js
+++ Oreilly/Project1/src/index.js
@@ -3,12 +3,19 @@ import * as Vue from "vue/dist/vue.esm-bundler.js";
 const app = Vue.createApp({
 
     template: `
-    <h1>{{msg}}</h1>
+    <h1>{{count}}</h1>
+    <button v-on:click="increment">Increment</button>
     `,
 
     data() {
         return {
-            msg: 'Hello World'
+            count: 0
+        }
+    },
+
+    methods: {
+        increment() {
+            this.count += 1;
         }
     }
 })
