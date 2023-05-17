d00b3322b62b5af9dc1b967fd8da2d9fa86a913c
listenToRipley <natalie.m.kendrick@gmail.com>
2023-05-17 15:46:33 -0400

    shorten and create a loop

--- Notes/Logic.md
+++ Notes/Logic.md
@@ -1,2 +1,13 @@
-# Logical 
+# Logical
 
+Also known as "directive" in Vue.
+
+## If
+
+`v-if` provides the functional check against what is possible.
+
+It is important to note that `v-else` should be right next to `v-if` or it will not work.
+
+## Looping
+
+`v-for`
--- Oreilly/Project1/src/index.js
+++ Oreilly/Project1/src/index.js
@@ -5,17 +5,22 @@ const app = Vue.createApp({
     template: `
     <h1>{{count}}</h1>
     <button v-on:click="increment">Increment</button>
-    <div v-if=isEven(count)>
+    <div v-if=isEven>
         Even
     </div>
     <div v-else>
         Odd
     </div>
+
+    <div v-for="number in numbers">
+        <div>{{number}}</div>
+    </div>
     `,
 
     data() {
         return {
-            count: 0
+            count: 0,
+            numbers: [1,2,3,4,5,6,7,8,9,10]
         }
     },
 
@@ -24,8 +29,8 @@ const app = Vue.createApp({
             this.count += 1;
         },
 
-        isEven(num) {
-            return num % 2 === 0 ? true : false;
+        isEven() {
+            return this.count % 2 === 0 ? true : false;
         }
     }
 })
